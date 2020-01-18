8490c88b1671df278c92f6b4c940cf4877c33995
TheGreatNectarine <margal.nik@gmail.com>
2020-01-18 13:07:07 +0200

    mock json server

--- app.py
+++ app.py
@@ -8,5 +8,11 @@ def hello_world():
     return 'Hello World!'
 
 
+@app.route("/buildings")
+def get_buildings():
+    with open("static/buildings.json", "r") as buildings:
+        return buildings.read()
+
+
 if __name__ == '__main__':
     app.run()
